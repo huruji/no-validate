6b604cf5bd0de678251ef0c6ad129ecc3860b842
huruji <594613537@qq.com>
2018-11-09 16:33:49 +0800

    add test and fix bug

--- src/rules/string.js
+++ src/rules/string.js
@@ -5,7 +5,7 @@ const ipRegex = /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-
 const uaIOSRegex = /((iPad)|(iPod)|iPhone).*OS\s([\d_]+)/
 const uaAndroidRegex = /(Android);?[\s/]+([\d.]+)?/
 const uaWXRegex = /micromessenger/i
-const uaQQRegex = /mqqbrowser/i
+const uaQQRegex = /m?qq(browser)?/i
 const urlRegex = /^((?:(?:https?|ftps?):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
 const notBase64Regex = /[^A-Z0-9+/=]/i
 
--- test/rules.string.test.js
+++ test/rules.string.test.js
@@ -6,6 +6,10 @@ import validation from '../index'
 
 const noV = validation
 
+const ios = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1'
+const android = 'Mozilla/5.0 (Linux; U; Android 6.0.1; zh-cn; MI 5s Build/MXB48T) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.128 Mobile Safari/537.36 XiaoMi/MiuiBrowser/9.7.2'
+const wx = 'Mozilla/5.0 (Linux; Android 6.0.1; MI 5s Build/MXB48T; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044306 Mobile Safari/537.36 MicroMessenger/6.6.7.1320(0x26060739) NetType/WIFI Language/zh_CN'
+const qq = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15F79 QQ/7.8.8.420 V1_IPH_SQ_7.8.8_1_APP_A Pixel/1080 Core/UIWebView Device/Apple(iPhone 6sPlus) NetType/WIFI QBWebViewType/1'
 
 test('string rules email', () => {
   expect(noV().email().test('594613537@qq.com')).toBe(true);
@@ -66,4 +70,22 @@ test('string rules hex', () => {
 test('strin rules ip', () => {
   expect(noV().ip().test('8.8.8.8')).toBe(true)
   expect(noV().ip().test('192.168.1.1')).toBe(true)
+})
+
+test('string rules uaIOS', () => {
+  expect(noV().uaIOS().test(ios)).toBe(true)
+  expect(noV().uaIOS().test(android)).toBe(false)
+})
+
+test('string rules uaAndroid', () => {
+  expect(noV().uaAndroid().test(android)).toBe(true)
+  expect(noV().uaAndroid().test(ios)).toBe(false)
+})
+
+test('string rules uaWX', () => {
+  expect(noV().uaWX().test(wx)).toBe(true)
+})
+
+test('string rules uaQQ', () => {
+  expect(noV().uaQQ().test(qq)).toBe(true)
 })
