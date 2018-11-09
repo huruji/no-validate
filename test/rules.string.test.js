4c8600f51559e7cc2b8eebd1194ac32b8ee52370
huruji <594613537@qq.com>
2018-11-09 15:36:15 +0800

    fix bug and add test

--- test/rules.string.test.js
+++ test/rules.string.test.js
@@ -2,8 +2,36 @@ import validation from '../index'
 
 const noV = validation
 
+
 test('string rules email', () => {
   expect(noV().email().test('594613537@qq.com')).toBe(true);
   expect(noV().email().test('nov_support@163.com')).toBe(true);
   expect(noV().email().test('huruji3#gmail.com')).toBe(false);
+})
+
+test('string rules lower', () => {
+  expect(noV().lower().test('nov')).toBe(true);
+  expect(noV().lower().test('noV')).toBe(false);
+})
+
+test('string rules upper', () => {
+  expect(noV().upper().test('nov')).toBe(false);
+  expect(noV().upper().test('NOV')).toBe(true);
+})
+
+test('string rules phoneNumber', () => {
+  const phone = '18020481871';
+  expect(noV().phoneNumber().test(phone)).toBe(true);
+  expect(noV().phoneNumber().test(phone.substr(1))).toBe(false);
+})
+
+test('string rules url', () => {
+  const urls = [
+    'http://www.baidu.com',
+    'https://google.com',
+    '//jd.com'
+  ]
+  urls.forEach((e) => {
+    expect(noV().url().test(e)).toBe(true);
+  })
 })
